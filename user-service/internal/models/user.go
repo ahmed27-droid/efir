c05178eb458a12527c0792e95a7538483af05a3c
akhmed <dombayev.akhmed@bk.ru>
2026-02-16 17:05:34 +0300

    fix:dto

--- user-service/internal/models/user.go
+++ user-service/internal/models/user.go
@@ -21,16 +21,3 @@ type User struct {
 	IsActive     bool   `gorm:"default:true"`
 }
 
-type CreateUserRequest struct {
-	Email     string	`json:"email" validate:"required,email"`
-	Username  string	`json:"username" validate:"required,min=3,max=20"`
-	Password  string	`json:"password" validate:"required,min=6,max=23"`
-	FirstName string	`json:"first_name" validate:"required,min=4,max=14"`
-	LastName  string	`json:"last_name" validate:"required,min=3,max=12"`
-}
-
-type UpdateUserRequest struct {
-	FirstName string	`json:"first_name"`
-	LastName  string	`json:"last_name"`
-	Username  string	`json:"username"`
-}
