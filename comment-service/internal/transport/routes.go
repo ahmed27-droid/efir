bc485a47aa48f5e9290b22d2c7c717148e49944e
ahmed27-droid <arhimedme@gmail.com>
2026-02-23 10:28:51 +0300

    update routes

--- comment-service/internal/transport/routes.go
+++ comment-service/internal/transport/routes.go
@@ -7,12 +7,12 @@ func RegisterRoutes(
 	commentTransport *CommentTransport,
 	reactionTransport *ReactionTransport,
 ) {
-	router.GET("/comments", commentTransport.ListComments)
+	router.GET("posts/:id/comments", commentTransport.ListComments)
 	router.POST("/posts/:id/comments", commentTransport.CreateComment)
 	router.PATCH("/comments/:id", commentTransport.UpdateComment)
 	router.DELETE("/comments/:id", commentTransport.DeleteComment)
 
-	router.GET("/reactions", reactionTransport.ListReaction)
+	router.GET("/posts/:id/reactions", reactionTransport.ListReaction)
 	router.POST("/posts/:id/reactions", reactionTransport.CreateReaction)
 	router.PATCH("/reactions/:id", reactionTransport.UpdateReaction)
 	router.DELETE("/reactions/:id", reactionTransport.DeleteReaction)
