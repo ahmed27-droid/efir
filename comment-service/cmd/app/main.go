d033c3017d5c85fe42cb9978883d6386999fe882
ahmed27-droid <arhimedme@gmail.com>
2026-02-23 10:02:08 +0300

    implemented routes

--- comment-service/cmd/app/main.go
+++ comment-service/cmd/app/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"comment-Service/internal/cache"
+	"comment-Service/internal/client"
 	"comment-Service/internal/config"
 	"comment-Service/internal/models"
+	"comment-Service/internal/repository"
+	"comment-Service/internal/services"
+	"comment-Service/internal/transport"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,14 +18,26 @@ func main() {
 
 	db.AutoMigrate(&models.Comment{}, &models.Reaction{})
 
-	//rdb := config.NewRedis()
+	rdb := config.NewRedis()
 
-	// broadcastClient := client.NewBroadcastClient("http://localhost:8081")
-	// cache := cache.NewRedisCache(rdb)
-	// commentRepo := repository.NewCommentRepository(db)
-	// commentService := services.NewCommentServices(commentRepo, cache, *broadcastClient)
+	broadcastClient := client.NewBroadcastClient("http://localhost:8081")
+	cache := cache.NewRedisCache(rdb)
+
+	commentRepo := repository.NewCommentRepository(db)
+	commentService := services.NewCommentServices(commentRepo, cache, *broadcastClient)
+	commentTransport := transport.NewCommentTransport(commentService)
+
+	reactionRepo := repository.NewReactionRepository(db)
+	reactionService := services.NewReactionService(reactionRepo, cache, *broadcastClient)
+	reactionTransport := transport.NewReactionTransport(reactionService)
 
 	router := gin.Default()
+
+	transport.RegisterRoutes(
+		router,
+		commentTransport,
+		reactionTransport,
+	)
 
 	router.Run()
 }
